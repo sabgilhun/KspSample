69a9f0485ee3670d1d73affa782f639bc23d4893
sabgilhun <38038123+sabgilhun@users.noreply.github.com>
2021-08-23 20:33:30 +0900

    Update MyProcessor.kt

--- processor/src/main/kotlin/org/sabgil/processor/MyProcessor.kt
+++ processor/src/main/kotlin/org/sabgil/processor/MyProcessor.kt
@@ -2,6 +2,7 @@ package org.sabgil.processor
 
 import com.google.devtools.ksp.processing.*
 import com.google.devtools.ksp.symbol.*
+import com.google.devtools.ksp.validate
 import org.sabgil.annotation.MyAnnotation
 import java.io.OutputStream
 
@@ -20,8 +21,6 @@ class MyProcessor(
             .getSymbolsWithAnnotation(MyAnnotation::class.java.canonicalName) // annotation 원하는 심볼을 필터링 할 수 있습니다.
             .filterIsInstance(KSClassDeclaration::class.java)
 
-        logger.warn("round: $round, ${symbols.toList().size}")
-
         symbols.iterator().hasNext()
         if (!symbols.iterator().hasNext()) {
             return emptyList()
@@ -31,144 +30,46 @@ class MyProcessor(
         val file: OutputStream = codeGenerator.createNewFile(
             dependencies = Dependencies(false),
             packageName = "com.sabgil",
-            fileName = "GeneratedFile$round"
+            fileName = "GeneratedFile"
         )
 
-        if (round++ < 10) {
-            file += "package com.sabgil\n\nimport org.sabgil.annotation.MyAnnotation\n\n@MyAnnotation\ninterface MyInterface$round"
+        // interface 에서만 사용될 수 있도록 강제
+        if (symbols.any { it.classKind != ClassKind.INTERFACE }) {
+            logger.error("인터페이스만 가능!!")
         }
-//
-//        symbols.forEach { it.accept(Visitor(file), "start") }
-//
-//        val file: OutputStream = codeGenerator.createNewFile(
-//            dependencies = Dependencies(false),
-//            packageName = "com.morfly",
-//            fileName = "GeneratedFunctions"
-//        )
-//        file.write("package com.morfly\n".toByteArray())
-//        symbols.forEach { it.accept(Visitor(file), Unit) }
-//
-//        file.close()
-//        print("build....")
-//        val unableToProcess = symbols.filterNot { it.validate() }.toList()
-//        return unableToProcess
-        return if (round < 10) {
-            symbols.toList()
-        } else {
-            emptyList()
-        }
+
+        file += "package com.sabgil\n"
+
+        val visitor = Visitor(file)
+        symbols.forEach { it.accept(visitor, Unit) }
+
+        file.close()
+
+        return symbols.filterNot { it.validate() }.toList()
     }
 
-    var round = 0
+    inner class Visitor(
+        private val file: OutputStream
+    ) : KSVisitorVoid() {
 
-    inner class Visitor(private val file: OutputStream) : KSVisitor<String, Int> {
-        override fun visitAnnotated(annotated: KSAnnotated, data: String): Int {
-            TODO("Not yet implemented")
+        override fun visitClassDeclaration(classDeclaration: KSClassDeclaration, data: Unit) {
+            file += "\n" +
+                    "fun ${classDeclaration.simpleName.getShortName().lowercase()}() {\n"
+
+            file += "\tprint(\n"
+            file += "\t\t\"\"\"\n"
+
+            classDeclaration.getAllProperties().forEach {
+                it.accept(this, Unit)
+            }
+
+            file += "\t\t\"\"\"\n"
+            file += "\t)\n"
+            file += "}"
         }
 
-        override fun visitAnnotation(annotation: KSAnnotation, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitCallableReference(reference: KSCallableReference, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitClassDeclaration(
-            classDeclaration: KSClassDeclaration,
-            data: String
-        ): Int {
-            logger.warn("class")
-            return 0
-        }
-
-        override fun visitClassifierReference(reference: KSClassifierReference, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitDeclaration(declaration: KSDeclaration, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitDeclarationContainer(
-            declarationContainer: KSDeclarationContainer,
-            data: String
-        ): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitDynamicReference(reference: KSDynamicReference, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitFile(file: KSFile, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitFunctionDeclaration(function: KSFunctionDeclaration, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitModifierListOwner(
-            modifierListOwner: KSModifierListOwner,
-            data: String
-        ): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitNode(node: KSNode, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitParenthesizedReference(
-            reference: KSParenthesizedReference,
-            data: String
-        ): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitPropertyAccessor(accessor: KSPropertyAccessor, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitPropertyDeclaration(property: KSPropertyDeclaration, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitPropertyGetter(getter: KSPropertyGetter, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitPropertySetter(setter: KSPropertySetter, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitReferenceElement(element: KSReferenceElement, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitTypeAlias(typeAlias: KSTypeAlias, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitTypeArgument(typeArgument: KSTypeArgument, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitTypeParameter(typeParameter: KSTypeParameter, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitTypeReference(typeReference: KSTypeReference, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitValueArgument(valueArgument: KSValueArgument, data: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun visitValueParameter(valueParameter: KSValueParameter, data: String): Int {
-            TODO("Not yet implemented")
+        override fun visitPropertyDeclaration(property: KSPropertyDeclaration, data: Unit) {
+            file += "\t\t${property.simpleName.asString()}: ${property.type.resolve().declaration.simpleName.asString()}\n"
         }
     }
 }
